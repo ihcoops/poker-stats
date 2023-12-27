74d969516b7e204a012311c586c861919063abf5
Isaac Cooper <isaaccooper10@gmail.com>
2023-12-27 00:40:12 -0500

    test

--- data/gameData.js
+++ data/gameData.js
@@ -4,6 +4,6 @@ export const games = [
         date: "12/26/2023",
         players: ["ISAAC_COOPER", "TYLER_YAP", "REED_WILLIAMSON", "JACK_FOX", "LUKE_WIELAND", "LUKE_WOLFORD", "JUSTIN_KIM"],
         in: [100, 150, 108, 100, 100, 80, 50],
-        out: [97, 0, 0, 204, 125, 247, 28]
+        out: [97, 0, 0, 204, 125, 247, 0]
     }
 ];
