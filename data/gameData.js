726d06f0ce07c4af7f4a64bf131f7ca44f59f200
Isaac Cooper <isaaccooper10@gmail.com>
2024-01-03 02:19:38 -0500

    1/2/2024

--- data/gameData.js
+++ data/gameData.js
@@ -5,5 +5,11 @@ export const games = [
         players: ["ISAAC_COOPER", "TYLER_YAP", "REED_WILLIAMSON", "JACK_FOX", "LUKE_WIELAND", "LUKE_WOLFORD", "JUSTIN_KIM"],
         in: [100, 150, 108, 100, 100, 80, 50],
         out: [97, 0, 0, 204, 125, 247, 28]
-    }
+    },
+    {
+        date: "1/2/2024",
+        players: ["ISAAC_COOPER", "LUKE_WIELAND", "MILES_ROBERTS", "AARON_HANRAHAN", "JACK_FOX", "TYLER_YAP", "LUKE_WOLFORD", "REED_WILLIAMSON", "BRYCE_TAYLOR"],
+        in: [100, 100, 200, 150, 100, 133, 100, 100, 100],
+        out: [190, 248, 0, 51, 140, 0, 190, 264, 0]
+    },
 ];
