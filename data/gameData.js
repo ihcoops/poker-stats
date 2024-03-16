2cba105888da8992097ab90fdfe9fb9b769b6e2d
Isaac Cooper <isaaccooper10@gmail.com>
2024-03-16 01:29:48 -0400

    3/16/2024

--- data/gameData.js
+++ data/gameData.js
@@ -1,15 +1,48 @@
 //game data
 export const games = [
-    {
-        date: "12/26/2023",
-        players: ["ISAAC_COOPER", "TYLER_YAP", "REED_WILLIAMSON", "JACK_FOX", "LUKE_WIELAND", "LUKE_WOLFORD", "JUSTIN_KIM"],
-        in: [100, 150, 108, 100, 100, 80, 50],
-        out: [97, 0, 0, 204, 125, 247, 28]
-    },
-    {
-        date: "1/2/2024",
-        players: ["ISAAC_COOPER", "LUKE_WIELAND", "MILES_ROBERTS", "AARON_HANRAHAN", "JACK_FOX", "TYLER_YAP", "LUKE_WOLFORD", "REED_WILLIAMSON", "BRYCE_TAYLOR"],
-        in: [100, 100, 200, 150, 100, 133, 100, 100, 100],
-        out: [190, 248, 0, 51, 140, 0, 190, 264, 0]
-    },
+  {
+    date: "12/26/2023",
+    players: [
+      "ISAAC_COOPER",
+      "TYLER_YAP",
+      "REED_WILLIAMSON",
+      "JACK_FOX",
+      "LUKE_WIELAND",
+      "LUKE_WOLFORD",
+      "JUSTIN_KIM",
+    ],
+    in: [100, 150, 108, 100, 100, 80, 50],
+    out: [97, 0, 0, 204, 125, 247, 28],
+  },
+  {
+    date: "1/2/2024",
+    players: [
+      "ISAAC_COOPER",
+      "LUKE_WIELAND",
+      "MILES_ROBERTS",
+      "AARON_HANRAHAN",
+      "JACK_FOX",
+      "TYLER_YAP",
+      "LUKE_WOLFORD",
+      "REED_WILLIAMSON",
+      "BRYCE_TAYLOR",
+    ],
+    in: [100, 100, 200, 150, 100, 133, 100, 100, 100],
+    out: [190, 248, 0, 51, 140, 0, 190, 264, 0],
+  },
+  {
+    date: "3/16/2024",
+    players: [
+      "LUKE_WOLFORD",
+      "JUSTIN_KIM",
+      "ISAAC_COOPER",
+      "TYLER_YAP",
+      "AARON_HANRAHAN",
+      "BRYCE_TAYLOR",
+      "LUKE_WIELAND",
+      "JACK_FOX",
+    ],
+    in: [100, 50, 100, 100, 150, 100, 100, 200],
+    out: [0, 64, 114, 0, 113, 123, 446, 40],
+  },
 ];
